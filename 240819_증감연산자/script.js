b569f73e69b62772d07dd1cc1d4118ba2f03260a
kyj102 <yain4746@naver.com>
2024-08-19 16:39:22 +0900

    증감연산자

---
+++ 240819_증감연산자/script.js
@@ -0,0 +1,35 @@
+// let num1 = 10;
+// let num2 = 5;
+// let result = 0;
+
+// // result = num1++ + --num2;
+
+// // result = --num1 - num2++;
+
+// result = num1++ * num2++;
+
+// console.log(num1, num2, result)
+
+// ===============================
+
+let num1 = 30;
+let num2 = 6;
+let num3 = 11;
+let result = 0;
+
+result = ++num1 + num2++ * num3--;
+console.log(`num1= ${num1}, num2=${num2}, num3=${num3}, result=${result}`);
+
+num1 -= 7;
+result = (num1-- / --num2) * ++num3;
+console.log(`num1= ${num1}, num2=${num2}, num3=${num3}, result=${result}`);
+
+num1 += num2++ * --num3;
+console.log(`num1= ${num1}, num2=${num2}, num3=${num3}, result=${result}`);
+
+
+
+
+
+
+
