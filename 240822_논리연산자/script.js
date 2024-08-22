11e43682a701d020783f8e7323f274e33ae9f7e0
kyj102 <yain4746@naver.com>
2024-08-22 16:42:06 +0900

    논리연산자

---
+++ 240822_논리연산자/script.js
@@ -0,0 +1,16 @@
+const a = 10;
+const b = 20;
+const m = 30;
+const n = 40;
+let result;
+result = a > b || b >= m || m > n;
+console.log(result);
+result = a > b || b >= m || m <= n;
+console.log(result);
+result = a <= b && b >= m && m <= n;
+console.log(result);
+result = a <= b && b <= m && m <= n;
+result = !(a > b);
+console.log(result);
+result = a <= b && b <= m && m <= n;
+console.log(result);
